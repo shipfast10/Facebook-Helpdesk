8b5d3aa9c846a07063146de61e5f421c3e3f8461
Bhavya Agarwal <bhavyaagarwal675@gmail.com>
2025-06-21 14:06:23 +0530

    Ensure CSS is imported correctly

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Login from "./components/Login";
 import Register from "./components/Register";
 import ConnectFb from "./components/ConnectFb";
+import './App.css';
 
 function App() {
   return (
